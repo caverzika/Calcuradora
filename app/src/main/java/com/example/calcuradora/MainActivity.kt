86d533f7abd4b897d46b455099116c9830c7ccfa
Gabriel Caverzan <caverzang@gmail.com>
2024-04-01 12:26:37 -0300

    TesteFinal1

--- app/src/main/java/com/example/calcuradora/MainActivity.kt
+++ app/src/main/java/com/example/calcuradora/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calcuradora
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,11 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calcuradora.databinding.ActivityMainBinding
 import com.example.calcuradora.ui.theme.CalcuradoraTheme
+import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -89,8 +92,13 @@ class MainActivity : ComponentActivity() {
         }
 
         binding.buttonApagar.setOnClickListener {
+            resultado.text = resultado.text.dropLast(1)
+        }
 
-            
+        binding.buttonIgual.setOnClickListener {
+            val res = Expression(resultado.text.toString()).calculate()
+
+            binding.resultResultadoCalculado.text = res.toString()
         }
     }
 }
